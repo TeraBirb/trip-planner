18e92999789bae82d1c0e62e4ea189e8bc010268
Isaiah <isaiah.g.callano@gmail.com>
2024-02-21 10:38:32 -0800

    Add activities to manifest, add layout context to activities

--- app/src/main/java/com/example/d308_mobile_application_development_android/Activities/VacationDetails.java
+++ app/src/main/java/com/example/d308_mobile_application_development_android/Activities/VacationDetails.java
@@ -2,5 +2,15 @@ package com.example.d308_mobile_application_development_android.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+import com.example.d308_mobile_application_development_android.R;
+
 public class VacationDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacation_details);
+    }
 }
--- app/src/main/java/com/example/d308_mobile_application_development_android/Activities/VacationList.java
+++ app/src/main/java/com/example/d308_mobile_application_development_android/Activities/VacationList.java
@@ -2,5 +2,15 @@ package com.example.d308_mobile_application_development_android.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+import com.example.d308_mobile_application_development_android.R;
+
 public class VacationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacation_list);
+    }
 }
