3a876483eb03cb0abacc941823b571831fa65c32
Isaiah Callano <isaiah.g.callano@gmail.com>
2024-02-22 17:18:08 -0800

    4.  Include the following details for each excursion:
    •  The excursion title (e.g., Snorkeling, Hiking, Bus Tour, Cooking Lesson)
    
    •  The excursion date

--- app/src/main/java/com/example/d308_mobile_application_development_android/Activities/ExcursionDetails.java
+++ app/src/main/java/com/example/d308_mobile_application_development_android/Activities/ExcursionDetails.java
@@ -2,15 +2,48 @@ package com.example.d308_mobile_application_development_android.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.d308_mobile_application_development_android.Database.Repository;
 import com.example.d308_mobile_application_development_android.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class ExcursionDetails extends AppCompatActivity {
+
+    int excursionID;
+    int vacationID;
+    String excursionTitle;
+    String excursionDate;
+    EditText editTitle;
+    EditText editDate;
+    Repository repository;
+    DatePickerDialog.OnDateSetListener dateListener;
+    Calendar myCalendar;
+    SimpleDateFormat sdf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excursion_details);
+
+        repository = new Repository(getApplication());
+        excursionID = getIntent().getIntExtra("id", -1);
+        excursionTitle = getIntent().getStringExtra("name");
+        excursionDate = getIntent().getStringExtra("date");
+        vacationID = getIntent().getIntExtra("vacID", -1);
+
+        editTitle = findViewById(R.id.editTextExcursionTitle);
+        editDate = findViewById(R.id.editTextExcursionDate);
+
+        editTitle.setText(excursionTitle);
+        editDate.setText(excursionDate);
+
+        sdf = new SimpleDateFormat("MM/dd/yy", Locale.US);
     }
 }
